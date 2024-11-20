b50ae67115ac419a923f3b5f92b825fa0a172d64
bulanmurela <bulanaputrim@gmail.com>
2024-11-20 22:55:03 +0700

    add BI analysis notebook

--- dags/etl_pipeline_dag.py
+++ dags/etl_pipeline_dag.py
@@ -6,21 +6,18 @@ from nbconvert.preprocessors import ExecutePreprocessor
 import nbformat
 
 # Define the paths for the notebook
-NOTEBOOK_PATH = '/opt/airflow/notebooks/ETL_Pipeline.ipynb'
-OUTPUT_NOTEBOOK_PATH = '/opt/airflow/notebooks/output/ETL_Pipeline_Output.ipynb'
+NOTEBOOK_PATH = '/opt/airflow/dags/notebooks/ETL_Pipeline.ipynb'
+OUTPUT_NOTEBOOK_PATH = '/opt/airflow/dags/notebooks/output/ETL_Pipeline_Output.ipynb'
 
 # Function to run the entire ETL pipeline notebook using nbconvert
 def run_etl_notebook():
     # Baca notebook
     with open(NOTEBOOK_PATH) as f:
         nb = nbformat.read(f, as_version=4)
-
     # Tentukan preprocessor
     ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
-
     # Jalankan notebook
     ep.preprocess(nb, {'metadata': {'path': '/opt/airflow/notebooks/'}})
-
     # Simpan hasilnya jika diperlukan
     with open(OUTPUT_NOTEBOOK_PATH, 'w') as f:
         nbformat.write(nb, f)
